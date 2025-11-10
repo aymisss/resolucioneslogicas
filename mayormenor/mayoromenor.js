a5c5a2368c32a9fde1af559927dc39ba24cff3b9
aymisss <vallecilloaymara@gmail.com>
2025-11-10 18:08:22 -0300

    mayormenor
    mayormenor

---
+++ mayormenor/mayoromenor.js
@@ -0,0 +1,11 @@
+let num1=parseInt(prompt("Ingrese primer numero"));
+let num2 =parseInt(prompt("Ingrese segundo numero"));
+if( num1>num2 ){
+    alert ("elnumero" + num1 + "es mayor");
+}if ( num1<num2){
+    alert("elnumero" + num2 + "es mayor");
+}if(num1===num2);{
+    alert("los numeros ingresados son iguales");
+}
+
+
