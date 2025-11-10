c4582609ca83f4e3c73b21a8017139f03fa92b8c
aymisss <vallecilloaymara@gmail.com>
2025-11-10 18:19:13 -0300

    ciclo
    ciclo

---
+++ ciclo for/index.js
@@ -0,0 +1,5 @@
+for(let i=1; i<=7; i++) {
+    console.log("vamos por el " + i);
+    //cuerpo
+}
+console.log("el ciclo terminó");
---
+++ ciclo while/index.js
@@ -0,0 +1,10 @@
+//iniciamos variable
+let puntos=0;
+//condición que se ejecuta mientras sea verdadera
+while (puntos <50) {
+    //cuerpo
+    console.log ("sumando puntos... total actual:" + puntos);
+    //alteración
+    puntos +=10
+}
+console.log ("meta alcanzada, total de puntos:" + puntos);
